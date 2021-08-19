1e83011e5daa7c3e1bec053a2e6e3dc3700c5b9e
VAISHNAVI-R-V <83284920+VAISHNAVI-R-V@users.noreply.github.com>
2021-08-19 19:20:56 +0530

    Added : Player checks for a

---
+++ SnackAndLadder.java
@@ -0,0 +1,29 @@
+public class SnackAndLadder {
+    public static final int noPlay = 0;
+    public static final int ladder = 1;
+    public static final int snake = 2;
+
+    public static void main(String[] args) {
+        int initial_position = 0;
+        int roll_die = (int) ((Math.floor(Math.random() * 10)) % 6) + 1;
+        int check_option = (int) ((Math.floor(Math.random() * 10)) % 3);
+        switch (check_option) {
+            case noPlay:
+                initial_position = 0;
+                System.out.println("Its a NO PLAY! Player stays in the same position!");
+                break;
+            case ladder:
+                initial_position += roll_die;
+                System.out.println("Its a LADDER! Player moves ahead by " + roll_die );
+                break;
+            case snake:
+                initial_position -= roll_die;
+                System.out.println("Its a SNAKE! Player moves behind by " + roll_die );
+                break;
+            default:
+                System.out.println("Error! Please check!");
+                break;
+        }
+        System.out.println("Current position of the player in the game is: " + initial_position);
+    }
+}
