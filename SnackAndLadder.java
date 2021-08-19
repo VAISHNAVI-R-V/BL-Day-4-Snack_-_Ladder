6279bd17ee14ffe35f79ca0cb94c2e2088ae6dcb
VAISHNAVI-R-V <83284920+VAISHNAVI-R-V@users.noreply.github.com>
2021-08-19 19:17:09 +0530

    Added : Initial Position of a Player.

---
+++ SnackAndLadder.java
@@ -0,0 +1,8 @@
+public class SnackAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to SnakeAndLadder Game");
+        // Single Player
+        int position = 0;
+        System.out.println("Player start position : " + position);
+    }
+}
