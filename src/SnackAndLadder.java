06e64dae51382b3b257a8548ed509ecb4012caf0
VAISHNAVI-R-V <vaishucv14@gmail.com>
2021-08-19 18:17:36 +0530

    Added: Die Rolls

--- src/SnackAndLadder.java
+++ src/SnackAndLadder.java
@@ -1,6 +1,13 @@
 public class SnackAndLadder {
     public static void main(String[] args) {
-        System.out.println("Welcome to Snake and Ladder Game");
+        System.out.println("Welcome to Snake And Ladder Game");
+        // Player Start Position
+        int position = 0;
+        System.out.println("Player start position : " + position);
+
+        // Dice value i.e between 1 to 6
+        int Roll_Die = (int) (Math.floor((Math.random() * 10 )) % 6 ) + 1;
+        System.out.println("Rolled die :  " +Roll_Die);
     }
 }
 
